930a878d65db61ecdc00a69c4f8418fe51233a35
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-08-31 15:22:29 +1000

    Created HireItem.kt and HireItemViewModel.kt

---
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItem.kt
@@ -0,0 +1,20 @@
+package com.example.cp3407_assignment.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cp3407_assignment.R
+
+class HireItem : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hire_item, container, false)
+    }
+}
---
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItemViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cp3407_assignment.ui
+
+import androidx.lifecycle.ViewModel
+
+class HireItemViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
