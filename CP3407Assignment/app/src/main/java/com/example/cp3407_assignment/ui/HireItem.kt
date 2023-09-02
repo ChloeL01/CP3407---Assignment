992e544843d06f62f09e76f0ce8fe414b4d36961
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-09-02 15:38:06 +1000

    Update user permission in AndroidManifest.xml

--- CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItem.kt
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItem.kt
@@ -1,11 +1,10 @@
 package com.example.cp3407_assignment.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.cp3407_assignment.R
 
 class HireItem : Fragment() {
