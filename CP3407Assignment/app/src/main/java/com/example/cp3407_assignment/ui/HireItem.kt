830ed30a66351746d51c80ab47abd6b42d9bd05d
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-09-03 12:21:39 +1000

    Set up bindings for HireItem.kt. Handle key events EditTexts

--- CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItem.kt
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItem.kt
@@ -1,19 +1,69 @@
 package com.example.cp3407_assignment.ui
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.cp3407_assignment.R
+import com.example.cp3407_assignment.databinding.FragmentHireItemBinding
 
 class HireItem : Fragment() {
+
+    private val viewModel: HireItemViewModel by viewModels()
+    private lateinit var binding: FragmentHireItemBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_hire_item, container, false)
+        //Inflate layout for fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_hire_item, container, false)
+
+        return binding.root
+    }
+
+    @Override
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    @Override
+    override fun onResume() {
+        super.onResume()
+        binding.lifecycleOwner = this
+
+        binding.name.setOnKeyListener{view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+        binding.description.setOnKeyListener{view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide keyboard
+            val inputMethodManager =
+                view.context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    @Override
+    override fun onPause() {
+        super.onPause()
     }
 }
--- CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItemViewModel.kt
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/HireItemViewModel.kt
@@ -3,5 +3,5 @@ package com.example.cp3407_assignment.ui
 import androidx.lifecycle.ViewModel
 
 class HireItemViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+   //
 }
