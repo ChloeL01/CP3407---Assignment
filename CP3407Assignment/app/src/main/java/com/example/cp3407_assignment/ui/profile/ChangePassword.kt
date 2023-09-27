2c75da790cad3262c19418cd6171bd3ae910f153
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-09-27 14:52:46 +1000

    Starting to add logic for checking passwords entered.

--- CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/profile/ChangePassword.kt
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/profile/ChangePassword.kt
@@ -1,11 +1,16 @@
 package com.example.cp3407_assignment.ui.profile
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.cp3407_assignment.R
 import com.example.cp3407_assignment.databinding.FragmentChangePasswordBinding
 
@@ -14,28 +19,67 @@ class ChangePassword : Fragment() {
 
     private lateinit var binding: FragmentChangePasswordBinding
 
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            // Not needed in this case
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            // Check if all EditText fields have input
+            binding.confirmNewPasswordBtn.isEnabled = checkAllEditTextsNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            // Not needed in this case
+        }
+    }
+
+    private fun checkAllEditTextsNotEmpty(): Boolean {
+        return binding.currentPassword.toString().isNotEmpty() && binding.newPassword.toString()
+            .isNotEmpty() && binding.confirmNewPassword.toString().isNotEmpty()
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
 
-        binding = DataBindingUtil.inflate(inflater,  R.layout.fragment_change_password, container,false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_change_password, container, false)
 
-       return binding.root
+        return binding.root
     }
+
 
     override fun onResume() {
         super.onResume()
 
-        binding.confirmNewPasswordBtn.setOnClickListener {
-            validateNewPassword()
+
+        binding.currentPassword.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
         }
+        binding.newPassword.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+        binding.confirmNewPassword.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+        binding.currentPassword.addTextChangedListener(textWatcher)
+        binding.newPassword.addTextChangedListener(textWatcher)
+        binding.confirmNewPassword.addTextChangedListener(textWatcher)
     }
 
-    private fun validateNewPassword() {
 
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide keyboard
+            val inputMethodManager =
+                view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
-
-
 }
