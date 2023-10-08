405a44c4a9e3a1f2b4df4b4368ce5df5e3a4c690
Jess Grimes <jessica.grimes@my.jcu.edu.au>
2023-10-08 13:58:29 +1000

    Start change email UI, databinding

---
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/profile/ChangeEmail.kt
@@ -0,0 +1,25 @@
+package com.example.cp3407_assignment.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.cp3407_assignment.R
+import com.example.cp3407_assignment.databinding.FragmentChangePasswordBinding
+
+class ChangeEmail : Fragment() {
+   private lateinit var binding: FragmentChangePasswordBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_change_email, container, false)
+
+        return binding.root
+    }
+}
