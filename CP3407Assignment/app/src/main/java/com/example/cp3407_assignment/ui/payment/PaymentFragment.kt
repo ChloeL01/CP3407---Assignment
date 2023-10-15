fd39fa14a91b5875ae6c24e5d403721ca6670eec
AengusDavies <aengus.davies@my.jcu.edu.au>
2023-10-16 09:01:04 +1000

    Add payment files to project

---
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/payment/PaymentFragment.kt
@@ -0,0 +1,32 @@
+package com.example.cp3407_assignment.ui.payment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.cp3407_assignment.R
+
+class PaymentFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PaymentFragment()
+    }
+
+    private lateinit var viewModel: PaymentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_payment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[PaymentViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+
+}
