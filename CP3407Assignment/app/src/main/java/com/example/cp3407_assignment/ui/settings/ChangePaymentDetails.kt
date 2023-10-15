6473ee9d1e94165eecbecfb9f09e606641534c72
jgrimes91 <jessica.grimes@my.jcu.edu.au>
2023-10-15 17:28:52 +1000

    Navigation back from change payment details to profile page.

--- CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/settings/ChangePaymentDetails.kt
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/settings/ChangePaymentDetails.kt
@@ -1,14 +1,16 @@
 package com.example.cp3407_assignment.ui.settings
 
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cp3407_assignment.R
 import com.example.cp3407_assignment.databinding.FragmentChangePaymentDetailsBinding
-import com.google.android.gms.common.util.DataUtils
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ChangePaymentDetails : Fragment() {
     private lateinit var binding: FragmentChangePaymentDetailsBinding
@@ -29,8 +31,14 @@ class ChangePaymentDetails : Fragment() {
 
     override fun onResume() {
         super.onResume()
-
+        binding.submitChangesBtn.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext()).setTitle(
+                "Placeholder: Navigate to selection website"
+            )
+                .setMessage("Placeholder: Later implementation of handling change of payment details for the user.\nOptions: Card, Paypal, or Google Pay")
+                .setPositiveButton(
+                    "Go back to Profile"
+                ) { _, _ -> findNavController().navigate(R.id.action_changePaymentDetails_to_navigation_profile) }.show()
+        }
     }
-
-
 }
