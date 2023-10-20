e2c36899079ce24aee0d1ace7441137903ee64f4
KeziahF <keziah.furnell@my.jcu.edu.au>
2023-10-20 23:32:08 +1000

    Currently hired page linked to database - not correctly getting current user data (registering no user signed in)

--- CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/current_hire/CurrentHireFragment.kt
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/current_hire/CurrentHireFragment.kt
@@ -22,15 +22,19 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+
 class CurrentHireFragment : Fragment() {
 
     private var _binding: FragmentCurrentHireBinding? = null
-
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val db = Firebase.firestore
     private lateinit var firebaseFirestore: FirebaseFirestore
     private lateinit var storageReference: StorageReference
     private var imageUri: Uri? = null
 
-    private val db = Firebase.firestore
     private val dogDBRef = db.collection("Dogs")
 
     private val binding get() = _binding!!
@@ -39,8 +43,60 @@ class CurrentHireFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
 
+        /*val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            Toast.makeText(
+                context,
+                "How You doin'",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                context,
+                "No user here ",
+                Toast.LENGTH_SHORT
+            ).show()
+        }*/
+
+        /*
+                ///////////////////////////////////////////////////////////
+                val db = FirebaseFirestore.getInstance()
+                val currentUser = firebaseAuth.currentUser?.uid
+                val userRef = db.collection("Users").document(currentUser!!)
+                Toast.makeText(
+                    context,
+                    "Hello There "+currentUser,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                userRef.addSnapshotListener { docSnapshot, firebaseFireStoreException ->
+                    if (firebaseFireStoreException != null) {
+                        Toast.makeText(
+                            context,
+                            "Not found",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    if (docSnapshot != null && docSnapshot.exists()) {
+                        val userName = docSnapshot.getString("username")
+                        Toast.makeText(
+                            context,
+                            "Current user username: "+ userName,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Current user username: null",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        */
         val CurrentHireViewModel =
             ViewModelProvider(this).get(CurrentHireViewModel::class.java)
 
@@ -81,38 +137,57 @@ class CurrentHireFragment : Fragment() {
 
 
                 //db.collection("Dogs").document(Hiree).get()
-                   // .addOnSuccessListener { documentSnapshot ->
-                      //  if (documentSnapshot.exists()) {
-                       //     val hiree = documentSnapshot.getString(Hiree)
-                         //   Toast.makeText(
-                         //       context,
-                         //       "Hello World",
-                         //       Toast.LENGTH_SHORT
-                         //   ).show()
-                      //  }}
+                // .addOnSuccessListener { documentSnapshot ->
+                //  if (documentSnapshot.exists()) {
+                //     val hiree = documentSnapshot.getString(Hiree)
+                //   Toast.makeText(
+                //       context,
+                //       "Hello World",
+                //       Toast.LENGTH_SHORT
+                //   ).show()
+                //  }}
 
                 //if (hiree == "Keziah") {
-                    mAdapter?.setOnClickListener(object :
-                        CurrentlyHiringItemAdapter.OnClickListener {
-                        override fun onClick(position: Int, model: Dog) {
-                            val bundle =
-                                bundleOf(
-                                    "doggo_name" to model.doggo_name,
-                                    "imageUrl" to model.imageUrl,
-                                    "start_date" to model.hire_start_date,
-                                    "end_date" to model.hire_end_date,
-                                    "breed" to model.doggo_breed
-                                )
-                            findNavController().navigate(
-                                R.id.action_currently_Hiring_to_doggoInformation,
-                                bundle
+                mAdapter?.setOnClickListener(object :
+                    CurrentlyHiringItemAdapter.OnClickListener {
+                    override fun onClick(position: Int, model: Dog) {
+                        val bundle =
+                            bundleOf(
+                                "doggo_name" to model.doggo_name,
+                                "imageUrl" to model.imageUrl,
+                                "start_date" to model.hire_start_date,
+                                "end_date" to model.hire_end_date,
+                                "breed" to model.doggo_breed
                             )
-                        }
-                    })
-               // }
+                        findNavController().navigate(
+                            R.id.action_currently_Hiring_to_doggoInformation,
+                            bundle
+                        )
+                    }
+                })
+                // }
 
             }
             }
+
+    override fun onStart(){
+        super.onStart()
+        firebaseAuth = Firebase.auth
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser == null) {
+            Toast.makeText(
+                context,
+                "No user signed in",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                context,
+                "Hello There ",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
