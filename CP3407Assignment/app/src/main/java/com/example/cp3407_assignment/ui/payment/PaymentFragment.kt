896167d1df2c1e3c6a7e2f5b0451442168ed7d52
AengusDavies <aengus.davies@my.jcu.edu.au>
2023-10-22 15:58:17 +1000

    Add payment code and xml to PaymentFragment

--- CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/payment/PaymentFragment.kt
+++ CP3407Assignment/app/src/main/java/com/example/cp3407_assignment/ui/payment/PaymentFragment.kt
@@ -1,32 +1,69 @@
 package com.example.cp3407_assignment.ui.payment
 
-import android.os.Bundle
-import android.view.LayoutInflater
+import android.content.Context
+import android.text.InputType
+import android.util.AttributeSet
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.braintreepayments.cardform.view.CardForm
 import com.example.cp3407_assignment.R
+import com.example.cp3407_assignment.databinding.FragmentPaymentBinding
 
-class PaymentFragment : Fragment() {
+class PaymentFragment : AppCompatActivity() {
 
-    companion object {
-        fun newInstance() = PaymentFragment()
-    }
-
-    private lateinit var viewModel: PaymentViewModel
+    private lateinit var cardForm: CardForm
+    private lateinit var binding: FragmentPaymentBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        name: String,
+        context: Context,
+        attrs: AttributeSet
+    ): View {
+        binding = DataBindingUtil.setContentView(this, R.layout.fragment_payment)
+        initialiseCardForm(binding)
+        initialisePayment(binding)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this)[PaymentViewModel::class.java]
-        // TODO: Use the ViewModel
+    private fun initialisePayment(binding: FragmentPaymentBinding) {
+        binding.hirePayment.setOnClickListener {
+            if (cardForm.isValid) {
+                Toast.makeText(
+                    this@PaymentFragment, "Card Number: ${cardForm.cardNumber} \n" +
+                            "Card expiry date: ${cardForm.expirationDateEditText} \n" +
+                            "Card CVV: ${cardForm.cvv} \n" +
+                            "Card holder name: ${cardForm.cardholderName} \n" +
+                            "Card postal code: ${cardForm.postalCode} \n" +
+                            "Card mobile number: ${cardForm.mobileNumber} \n",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@PaymentFragment,
+                    "Please complete the payment form.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
+
+    private fun initialiseCardForm(binding: FragmentPaymentBinding) {
+        cardForm = binding.cardForm
+        cardForm.cardRequired(true)
+            .expirationRequired(true)
+            .cvvRequired(true)
+            .cardholderName(CardForm.FIELD_REQUIRED)
+            .postalCodeRequired(true)
+            .mobileNumberRequired(true)
+            .mobileNumberExplanation("Mobile Number required for SMS verification")
+            .actionLabel("Pay for Hire")
+            .setup(this@PaymentFragment)
+
+        cardForm.cvvEditText.inputType =
+            InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD
+    }
+
 
 }
